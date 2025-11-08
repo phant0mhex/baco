dc2ed1f3a5462dd25cfc69082329162047820b4d
phant0mhex <nd.managment@gmail.com>
2025-11-08 23:30:46 +0100

    maj script

--- script.sh
+++ script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then 
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-
- # Proceed with the build  echo "✅ - Build can proceed" 
-
-exit 1;else  
-
-# Don't build  echo "🛑 - Build cancelled" 
-
- exit 0;fi
+# L'instruction 'if' doit commencer sur une nouvelle ligne
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then 
+  # Proceed with the build  
+  echo "✅ - Build can proceed" 
+  exit 1;
+else  
+  # Don't build  
+  echo "🛑 - Build cancelled" 
+  exit 0;
+fi
